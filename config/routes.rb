1354fed953333151ccf1fe4e80a4c07f4b43837b
Ryan <ryansrogers85@gmail.com>
2019-04-09 07:59:21 -0400

    devise jwt auth controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
+  devise_for :users
   #get 'staticpages/home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "staticpages#home"
+
+
+  devise_for :users,
+             path: '',
+             path_names: {
+               sign_in: 'login',
+               sign_out: 'logout',
+               registration: 'signup'
+             },
+             controllers: {
+               sessions: 'sessions',
+               registrations: 'registrations'
+             }
 end
