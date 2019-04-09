d1290ca1afe8469d0784585a1d3d743d6ef163a1
Ryan <ryansrogers85@gmail.com>
2019-04-09 07:37:44 -0400

    Adding default route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  #get 'staticpages/home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "home#index"
+  root to: "staticpages#home"
 end
